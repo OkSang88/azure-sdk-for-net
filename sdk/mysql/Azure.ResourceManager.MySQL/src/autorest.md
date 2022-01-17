6278153e36a9447a21c85127c10b6eb4d03222c4
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-01-17 17:32:08 +0800

    create template

---
+++ sdk/mysql/Azure.ResourceManager.MySQL/src/autorest.md
@@ -0,0 +1,11 @@
+# Generated code configuration
+
+Run `dotnet build /t:GenerateCode` to generate code.
+
+``` yaml
+
+azure-arm: true
+require: https://raw.githubusercontent.com/Azure/azure-rest-api-specs/322d0edbc46e10b04a56f3279cecaa8fe4d3b69b/specification/mysql/resource-manager/readme.md
+ 
+
+```
---
+++ sdk/storsimple8000series/Azure.ResourceManager.StorSimple8000Series/tests/StorSimple8000SeriesManagementTestEnvironment.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.ResourceManager.StorSimple8000Series.Tests
+{
+    public class StorSimple8000SeriesManagementTestEnvironment : TestEnvironment
+    {
+    }
+}
